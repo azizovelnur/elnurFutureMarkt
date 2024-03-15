56b7fc9f04e0c8a6fb609fb3818b502cd020d2ca
elnur <elnurwebdev@gmail.com>
2024-03-15 18:20:38 +0400

    names of classes were changed

--- src/pages/AboutMe.tsx
+++ src/pages/AboutMe.tsx
@@ -5,48 +5,47 @@ import btnArrowWhite from "../assets/images/btnArrowWhite.svg";
 export const AboutMe: React.FC = () => {
   return (
     <div className="container">
-      <main className="consultingWrapper">
-        <div className="margin-block">
-          <div className="consulting">
-            <h1 className="consulting__title">
-              Создаю условия для вашего успеха
-            </h1>
+      <main className="consulting-wrapper">
+        <div className="consulting">
+          <h1 className="consulting__title">
+            Создаю условия для вашего успеха
+          </h1>
 
-            <p className="consulting__subtitle">
-              <span className="consulting__subtitle-text">
-                Когда ваше время и энергия лучше сфокусированы, стремление к
-                новым возможностям становится реальностью, ваш успех зависит от
-                ваших действий
-              </span>
-            </p>
+          <p className="consulting__subtitle">
+            <span className="consulting__subtitle-text">
+              Когда ваше время и энергия лучше сфокусированы, стремление к новым
+              возможностям становится реальностью, ваш успех зависит от ваших
+              действий
+            </span>
+          </p>
 
-            <div className="consulting__buttons">
-              <button className="consulting__btn">
-                <span>Записаться на консультацию</span>
+          <div className="consulting__buttons">
+            <button className="consulting__buttons-btn">
+              <span>Записаться на консультацию</span>
+              <img className="btnArrow" src={btnArrow} alt="" />
+            </button>
+            <button className="consulting__buttons-btnfree">
+              <span>Бесплатная консультация</span>
+              <img className="btnArrow" src={btnArrowWhite} alt="" />
+            </button>
+          </div>
 
-                <img className="btnArrow" src={btnArrow} alt="" />
-              </button>
-              <button className="consulting__btnFree">
-                <span>Бесплатная консультация</span>
-                <img className="btnArrow" src={btnArrowWhite} alt="" />
-              </button>
+          <div className="consulting__additional-information">
+            <div className="consulting__quantity">
+              <div className="consulting__quantity-number">130+</div>
+              <p className="consulting__quantity-text">
+                техник для достижения целей
+              </p>
             </div>
-
-            <div className="consulting__additionalInformation">
-              <div className="quantity">
-                <div className="quantity__number">130+</div>
-                <p className="quantity__text">техник для достижения целей</p>
-              </div>
-              <div className="percent">
-                <div className="percent__number">250%</div>
-                <p className="percent__text">
-                  увеличение личной продуктивности
-                </p>
-              </div>
+            <div className="consulting__percent">
+              <div className="consulting__percent-number">250%</div>
+              <p className="consulting__percent-text">
+                увеличение личной продуктивности
+              </p>
             </div>
           </div>
         </div>
-        <div className="mentor">
+        <div className="consulting__mentor-img">
           <img src={mentor} alt="image doesn't work" />
         </div>
       </main>
